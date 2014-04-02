566aea6303c59e2f9773b1a08e8d7a3edd9aceac
Last1Here <last1here@hotmail.co.uk>
2014-04-02 16:47:19 +0100

    Removed pointless function and rearranged placement code.

--- master/gridded.js
+++ master/gridded.js
@@ -62,79 +62,42 @@
 			console.log( '1' +this.width);
 			this.colWidth = this.width / this.col;
 
-			this.plotItems();
 			this.placeItems();
 		},
 
 		placeItems: function () {
 			var that = this;
 			largestPushDown = 0;
-			this.element.css("position", "relative");
-			this.element.find(this.items).each(function() {
-				var i = $(this);
-				if (!i.data('w')) {
-					i.data('w', 1);
-				}
 
-				if (!i.data('h')) {
-					i.data('h', i.data('w'));
-				}
-
-				// calculate width
-				var itemWidth = i.data('w') * that.colWidth - that.gutter;
-				itemWidth = itemWidth - parseInt(i.css("border-left-width")) - parseInt(i.css("border-right-width"));
-				itemWidth = itemWidth - parseInt(i.css("padding-left")) - parseInt(i.css("padding-right"));
-
-				var itemHeight = i.data('w') * that.colWidth - that.gutter;
-				itemHeight = itemHeight - parseInt(i.css("border-top-width")) - parseInt(i.css("border-bottom-width"));
-				itemHeight = itemHeight - parseInt(i.css("padding-top")) - parseInt(i.css("padding-bottom"));
-
-				i.css("position", "absolute");
-
-				i.width(itemWidth);
-				i.height(itemHeight);
-
-				i.css("left", i.data('pr') * that.colWidth + parseInt(that.element.css("padding-left")));
-				i.css("top", i.data('pd') * that.colWidth + parseInt(that.element.css("padding-top")));
-
-				if(largestPushDown < (i.data('pd') + i.data('w')))
-					largestPushDown = Number(i.data('pd')) + Number(i.data('h'));
-			});
-
-			this.element.css("height", largestPushDown * this.colWidth - this.gutter + parseInt(this.element.css('padding-top'))  + parseInt(this.element.css('padding-bottom')) );
-		},
-
-		plotItems: function() {
 			var that = this, rn = 0, cn = 0, ic = 1;
 			var grid = new Array([]);
-			var row = 0;
 
 			// create grid
 			for (var cl = this.col - 1; cl >= 0; cl--) {
 				grid[cl] = new Array();
 			}
 
+			this.element.css("position", "relative");
 			this.element.find(this.items).each(function() {
-				var i = $(this), width, push = {}, placed = false, r = 0;
-				if (i.data('w')) {
-					if (i.data('w') > that.col) {
-						i.data('w', that.col);
-					}
-					width = i.data('w');
-				} else {
-					width = 1;
+				var i = $(this), pos = {}, placed = false, row = 0;
+
+				if (!i.data('w')) 
 					i.data('w', 1);
-				}
+				else if (i.data('w') > that.col) 
+					i.data('w', that.col);
+
+				if (!i.data('h'))
+					i.data('h', i.data('w'));
 
 				while (placed == false) {
 					for (var c = 0; c <= that.col - 1 ; c++) {
 						var empty = true;
-						for (var w = 0; w <= width - 1 ; w++) {
+						for (var w = 0; w <= i.data('w') - 1 ; w++) {
 							pushCol = Number(c+w);
-							pushRow = Number(r+w);
+							pushRow = Number(row+w);
 
 							if (pushCol < that.col) {
-								if(grid[pushCol][r] != null && grid[c][pushRow] != null) {
+								if(grid[pushCol][row] != null && grid[c][pushRow] != null) {
 									empty = false;
 								}
 							} else {
@@ -143,26 +106,46 @@
 						}
 
 						if(empty == true) {
-							push.col = c;
-							push.row = r;
-							for (var w = 0; w <= width - 1 ; w++) {
-								for (var w2 = 0; w2 <= width - 1 ; w2++) {
-									grid[Number(c+w)][Number(r+w2)] = 1;
+							for (var w = 0; w <= i.data('w') - 1 ; w++) {
+								for (var h = 0; h <= i.data('h') - 1 ; h++) {
+									grid[Number(c+w)][Number(row + h)] = 1;
 								}
 							}
+
+							i.data('pr', c);
+							i.data('pd', row);
+							i.data('data-ic', ic);
+
+							var itemWidth = i.data('w') * that.colWidth - that.gutter;
+							itemWidth = itemWidth - parseInt(i.css("border-left-width")) - parseInt(i.css("border-right-width"));
+							itemWidth = itemWidth - parseInt(i.css("padding-left")) - parseInt(i.css("padding-right"));
+
+							var itemHeight = i.data('h') * that.colWidth - that.gutter;
+							itemHeight = itemHeight - parseInt(i.css("border-top-width")) - parseInt(i.css("border-bottom-width"));
+							itemHeight = itemHeight - parseInt(i.css("padding-top")) - parseInt(i.css("padding-bottom"));
+
+							i.css("position", "absolute");
+							i.width(itemWidth);
+							i.height(itemHeight);
+
+							i.css("left", c * that.colWidth + parseInt(that.element.css("padding-left")));
+							i.css("top", row * that.colWidth + parseInt(that.element.css("padding-top")));
+							if(largestPushDown < (row + i.data('h')))
+								largestPushDown = row + Number(i.data('h'));
+
 							placed = true;
 							break;
 						}
 					}
-					r += 1;
+					row += 1;
 				}
 
-				i.data('pr', push.col);
-				i.data('pd', push.row);
-				i.attr('data-ic', ic);
+
 
 				ic++;
 			});
+
+			this.element.css("height", largestPushDown * this.colWidth - this.gutter + parseInt(this.element.css('padding-top'))  + parseInt(this.element.css('padding-bottom')) );
 		}
 
 	};
